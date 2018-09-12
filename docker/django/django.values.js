d40a657dec905b4893f3a35d48dda66772aad72b
MSI\nicolas <n.tiesenhausen@gmail.com>
2018-09-12 18:21:49 +0200

    first commit

---
+++ docker/django/django.values.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    first_project_name: 'my_project_name',
+    port: '8000',
+    container_name: 'django_5qj6cl6pcjlzcdanj',
+};
---
+++ docker/postgres/postgres.values.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    database_name: 'my_project_name',
+    user: 'root',
+    password: 'root',
+    database_storage_location: './',
+    port: '5432',
+    container_name: 'postgres_5qj6cl6pcjlzcdank',
+};
